55f92228fc603a00a97c772970bc84130f88f8a3
Paulo Gabriel <pgj1085@gmail.com>
2020-06-02 14:09:27 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/curso/springbootmonbodb/SpringBootMonbodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.springbootmonbodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonbodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMonbodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/curso/springbootmonbodb/SpringBootMonbodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.springbootmonbodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonbodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
