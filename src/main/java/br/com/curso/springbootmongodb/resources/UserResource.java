e45f2fa371e8b91fcceaf1d8057b8b0d45144f0c
Paulo Gabriel <pgj1085@gmail.com>
2020-06-02 14:28:25 -0300

    Entidade User e seu Resource

---
+++ src/main/java/br/com/curso/springbootmongodb/resources/UserResource.java
@@ -0,0 +1,24 @@
+package br.com.curso.springbootmongodb.resources;
+
+import br.com.curso.springbootmongodb.model.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User u1 = new User("1", "Maria Brown", "maria@gmail.com");
+        User u2 = new User("2", "Alex Green", "alex@gmail.com");
+        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(u1, u2)));
+    }
+
+}
--- src/test/java/br/com/curso/springbootmongodb/SpringBootMonbodbApplicationTests.java
+++ src/test/java/br/com/curso/springbootmongodb/SpringBootMonbodbApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.curso.springbootmonbodb;
+package br.com.curso.springbootmongodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
